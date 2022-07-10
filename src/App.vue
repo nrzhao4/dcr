776c8891cf816e7b408a41ed9155df22b7ab8fb0
Nina Zhao <nina.r.zhao@gmail.com>
2022-07-10 00:13:22 -0400

    router navigates to collection and gets collection data

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
-import CollectionItem from "@/components/CollectionItem.vue";
 </script>
 
 <template>
---
+++ src/components/collections/CollectionDisplay.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from "axios";
+
+export default {
+  props: {
+    collectionId: String,
+  },
+  data() {
+    return {
+      collectionData: {},
+    };
+  },
+  mounted() {
+    axios
+      .get(`http://localhost:4000/collections/${this.collectionId}`)
+      .then((res) => (this.collectionData = res.data));
+  },
+};
+</script>
+
+<template>
+  <div>
+    <p>{{ collectionData }}</p>
+  </div>
+</template>
