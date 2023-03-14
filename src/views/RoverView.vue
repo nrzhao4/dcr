17e6db7bbb3ec9e50e39801ea9811cbdf55045b1
Nina Zhao <nina.zhao@smartex.ai>
2023-03-14 14:01:09 -0400

    fix rover command sent typo

--- src/views/RoverView.vue
+++ src/views/RoverView.vue
@@ -1,13 +1,13 @@
 <script>
-import axios from "axios";
-import RoverInfo from "@/components/rover/RoverInfo.vue";
+import axios from 'axios';
+import RoverInfo from '@/components/rover/RoverInfo.vue';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export default {
   data() {
     return {
-      sendRoverStartStatus: "",
+      sendRoverStartStatus: '',
     };
   },
   methods: {
@@ -23,17 +23,17 @@ export default {
         .post(url, data)
         .then((res) => {
           if (res.status === 200) {
-            console.log("successfully posted start command");
+            console.log('successfully posted start command');
             console.log(res.data);
-            this.sendRoverStartStatus = "Start command send";
+            this.sendRoverStartStatus = 'Start command sent';
           } else {
-            console.log("error");
-            this.sendRoverStartStatus = "Error sending start command";
+            console.log('error');
+            this.sendRoverStartStatus = 'Error sending start command';
           }
         })
         .catch((err) => {
-          console.log("error");
-          this.sendRoverStartStatus = "Error sending start command";
+          console.log('error');
+          this.sendRoverStartStatus = 'Error sending start command';
         });
     },
   },
@@ -46,7 +46,6 @@ export default {
       name="Penelope"
       date="5 March 2023"
       :status="sendRoverStartStatus"
-      @rover-start="handleRoverStart"
-    />
+      @rover-start="handleRoverStart" />
   </main>
 </template>
